5418da82d6da7c917a16f796c3dfa8b718a42209
nSedrickm <nsedrick101@gmail.com>
2020-09-10 05:08:07 +0100

    writing and reading from localStorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,23 +26,24 @@ window.addEventListener("load", function () {
     xhttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
         let response = JSON.parse(this.responseText);
-        console.log(response);
+        localStorage.setItem("weather_statistics", JSON.stringify(response));
+        const WeatherStats = JSON.parse(localStorage.getItem('weather_statistics'));
 
-        locationName.innerHTML = response.name;
-        date.innerHTML = new Date(response.dt * 1000).toDateString();
-        time.innerHTML = new Date(response.dt * 1000).toLocaleTimeString();
-        description.innerHTML = response.weather[0].description;
-        temp.innerHTML = response.main.temp;
-        minTemp.innerHTML = response.main.temp_min;
-        maxTemp.innerHTML = response.main.temp_max;
-        feelsLike.innerHTML = response.main.feels_like;
-        cloudiness.innerHTML = response.clouds.all;
-        windSpeed.innerHTML = response.wind.speed;
-        windDir.innerHTML = response.wind.deg;
-        humidity.innerHTML = response.main.humidity;
-        pressure.innerHTML = response.main.pressure;
-        weatherCond.innerHTML = response.weather[0].main;
-        visibility.innerHTML = response.visibility;
+        locationName.innerHTML = WeatherStats.name;
+        date.innerHTML = new Date(WeatherStats.dt * 1000).toDateString();
+        time.innerHTML = new Date(WeatherStats.dt * 1000).toLocaleTimeString();
+        description.innerHTML = WeatherStats.weather[0].description;
+        temp.innerHTML = WeatherStats.main.temp;
+        minTemp.innerHTML = WeatherStats.main.temp_min;
+        maxTemp.innerHTML = WeatherStats.main.temp_max;
+        feelsLike.innerHTML = WeatherStats.main.feels_like;
+        cloudiness.innerHTML = WeatherStats.clouds.all;
+        windSpeed.innerHTML = WeatherStats.wind.speed;
+        windDir.innerHTML = WeatherStats.wind.deg;
+        humidity.innerHTML = WeatherStats.main.humidity;
+        pressure.innerHTML = WeatherStats.main.pressure;
+        weatherCond.innerHTML = WeatherStats.weather[0].main;
+        visibility.innerHTML = WeatherStats.visibility;
       }
     };
     xhttp.open("GET", `https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=dd8fd220b843b73b78411eeae3a562eb`, true);
