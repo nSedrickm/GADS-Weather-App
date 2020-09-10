1d7ff0eb6aeb7ffb17b815e76b9856cf3eb9dc16
nSedrickm <nsedrick101@gmail.com>
2020-09-10 07:08:31 +0100

    search localstorage first

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ window.addEventListener("load", function () {
   //mapping stats containers 
 
   var locationName = document.getElementById("location_name");
+  var locationCode = document.getElementById("location_code");
   var date = document.getElementById("date");
   var time = document.getElementById("time");
   var description = document.getElementById("description");
@@ -18,45 +19,62 @@ window.addEventListener("load", function () {
   var humidity = document.getElementById("humidity");
   var pressure = document.getElementById("pressure");
   var weatherCond = document.getElementById("weather_cond");
-  var dewPoint = document.getElementById("dew_point");
   var visibility = document.getElementById("visibility");
+  var searchForm = document.getElementById("search_form");
 
-  ((function getWeather() {
+
+  function getForecast(location) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
         let response = JSON.parse(this.responseText);
-        localStorage.setItem("weather_statistics", JSON.stringify(response));
-        const WeatherStats = JSON.parse(localStorage.getItem('weather_statistics'));
-
-        locationName.innerHTML = WeatherStats.name;
-        date.innerHTML = new Date(WeatherStats.dt * 1000).toDateString();
-        time.innerHTML = new Date(WeatherStats.dt * 1000).toLocaleTimeString();
-        description.innerHTML = WeatherStats.weather[0].description;
-        temp.innerHTML = WeatherStats.main.temp;
-        minTemp.innerHTML = WeatherStats.main.temp_min;
-        maxTemp.innerHTML = WeatherStats.main.temp_max;
-        feelsLike.innerHTML = WeatherStats.main.feels_like;
-        cloudiness.innerHTML = WeatherStats.clouds.all;
-        windSpeed.innerHTML = WeatherStats.wind.speed;
-        windDir.innerHTML = WeatherStats.wind.deg;
-        humidity.innerHTML = WeatherStats.main.humidity;
-        pressure.innerHTML = WeatherStats.main.pressure;
-        weatherCond.innerHTML = WeatherStats.weather[0].main;
-        visibility.innerHTML = WeatherStats.visibility;
+        console.log(response);
+        localStorage.setItem(location, JSON.stringify(response));
+        showForecast(response);
       }
     };
-    xhttp.open("GET", `https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=dd8fd220b843b73b78411eeae3a562eb`, true);
+    xhttp.open("GET", `https://api.openweathermap.org/data/2.5/weather?q=` + location + `&APPID=dd8fd220b843b73b78411eeae3a562eb`, true);
     xhttp.send();
-  })());
+  };
 
+  function showForecast(statistics) {
+    locationName.innerHTML = statistics.name;
+    locationCode.innerHTML = statistics.sys.country;
+    date.innerHTML = new Date(statistics.dt * 1000).toDateString();
+    time.innerHTML = new Date(statistics.dt * 1000).toLocaleTimeString();
+    description.innerHTML = statistics.weather[0].description;
+    temp.innerHTML = statistics.main.temp;
+    minTemp.innerHTML = statistics.main.temp_min;
+    maxTemp.innerHTML = statistics.main.temp_max;
+    feelsLike.innerHTML = statistics.main.feels_like;
+    cloudiness.innerHTML = statistics.clouds.all;
+    windSpeed.innerHTML = statistics.wind.speed;
+    windDir.innerHTML = statistics.wind.deg;
+    humidity.innerHTML = statistics.main.humidity;
+    pressure.innerHTML = statistics.main.pressure;
+    weatherCond.innerHTML = statistics.weather[0].main;
+    visibility.innerHTML = statistics.visibility;
+  }
 
-  let searchForm = document.getElementById("search_form");
-  searchForm.addEventListener('submit', function (event) {
+  function searchForecast(event) {
     event.preventDefault();
     let searchLocationName = document.getElementById("search_field").value;
     console.log(searchLocationName)
+    let fetchedStats = JSON.parse(localStorage.getItem(searchLocationName));
+    console.log("logginfs", fetchedStats)
+    if (fetchedStats == null) {
+      getForecast(searchLocationName);
+    }
+    else if (fetchedStats.name == searchLocationName) {
+      showForecast(fetchedStats);
+    }
+    else {
+      getForecast(searchLocationName);
+    }
+  }
 
-  });
+  //default gets forecast for Bamenda Cameroon
+  getForecast("Bamenda");
+  searchForm.addEventListener('submit', searchForecast);
 
 });
